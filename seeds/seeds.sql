7b64c74583f8b07041914779cf10024fd5ec736e
LarkMoore <moore.lark@gmail.com>
2021-11-22 19:50:26 -0500

    seeds file

---
+++ seeds/seeds.sql
@@ -0,0 +1,62 @@
+INSERT INTO User (user_firstname)
+VALUES ("Jamal"),
+       ("Frank"),
+       ("Polly"),
+       ("Kim"),
+       ("Shane"),
+
+INSERT INTO User (user_lastname)
+VALUES ("Witherby"),
+       ("Fin"),
+       ("Cracker"),
+       ("Wall"),
+       ("Blue"),
+
+INSERT INTO Pet (pet_name)
+VALUES ("Pancake"),
+       ("Flippers"),
+       ("Biscuit"),
+       ("Spiderman"),
+       ("Bean"),
+
+INSERT INTO User (user_id)
+VALUES ("1234"),
+       ("5678"),
+       ("9009"),
+       ("1001"),
+       ("2002"),
+
+INSERT INTO Pet (pet_id)
+VALUES ("4321"),
+       ("8765"),
+       ("3003"),
+       ("4004"),
+       ("5005"),
+
+INSERT INTO User (user_email)
+VALUES ("j.wiz@gmail.com"),
+       ("f.fin@gmail.com"),
+       ("p.cracker@gmail.com"),
+       ("k.wall@gmail.com"),
+       ("s.blue@gmail.com"),
+
+INSERT INTO Pet (pet_age)
+VALUES ("2"),
+       ("3"),
+       ("12"),
+       ("10"),
+       ("9"),
+
+INSERT INTO Pet (pet_breed)
+VALUES ("Golden Retriever"),
+       ("Border Collie"),
+       ("Irish Setter"),
+       ("Yorkie"),
+       ("Pitbull/muttmix"),
+
+INSERT INTO Pet (pet_location)
+VALUES ("Cleveland, OH"),
+       ("Akron, OH"),
+       ("Las Vegas, NV"),
+       ("Boston, MA"),
+       ("Oakland, CA"),
