2160950a77177fddb317f8078eaf2b5ffa0303c4
Ikenna Nwauwa <icknwauwa15@gmail.com>
2021-11-24 19:18:16 -0500

    basic handlebars

---
+++ server.js
@@ -0,0 +1,7 @@
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+const routes = require('./controllers/');
