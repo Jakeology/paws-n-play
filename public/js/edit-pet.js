85db10beb38e520adb67744f0d833ebe888fd57b
Jake <xxjakexx14@gmail.com>
2021-12-06 18:44:14 -0500

    added delete script to remove a pet from the database

--- public/js/edit-pet.js
+++ public/js/edit-pet.js
@@ -7,7 +7,7 @@ async function editPetHandler(event) {
   const location = document.getElementById("pet-location").value.trim();
   const vaccinated = document.getElementById("pet-vaccinated").value.trim();
   const about = document.getElementById("pet-about").value.trim();
-  const pfp = document.getElementById("pet-pfp").value.trim();
+  const pfp = document.getElementById("edit-pet-pfp").value.trim();
 
   const id = window.location.toString().split("/")[window.location.toString().split("/").length - 1];
 
