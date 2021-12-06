072a8416a49f62bd29dc77c5b1c89d1115162fd2
Jake <xxjakexx14@gmail.com>
2021-12-05 22:16:35 -0500

    added dashboard routes, handlebars, and css

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const { Pet, User } = require("../models");
+
+router.get("/", (req, res) => {
+  Pet.findAll({
+    where: {
+      // use the ID from the session
+      owner_id: req.session.user_id,
+    },
+    attributes: ["id", "name", "age", "breed", "location"],
+    include: [
+      {
+        model: User,
+        attributes: ["id", "first_name", "last_name"],
+      },
+    ],
+  })
+    .then((dbPetData) => {
+      // serialize data before passing to template
+      const pets = dbPetData.map((pet) => pet.get({ plain: true }));
+      res.render("dashboard", { pets, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/add-pet", (req, res) => {
+  res.render("addpet", { loggedIn: true });
+});
+
+module.exports = router;
