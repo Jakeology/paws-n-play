f6218a0ae8f333c7b199b84fe043628d8686bdd8
Jake <xxjakexx14@gmail.com>
2021-12-05 21:22:52 -0500

    added home route to load all pet cards

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,29 @@
 const router = require("express").Router();
+const { Pet, User } = require("../models");
+
+router.get("/", (req, res) => {
+  Pet.findAll({
+    attributes: ["id", "name", "age", "breed", "location"],
+    include: [
+      {
+        model: User,
+        attributes: ["id", "first_name", "last_name"],
+      },
+    ],
+  })
+    .then((dbPetData) => {
+      // pass a single post object into the homepage template
+      const pets = dbPetData.map((pet) => pet.get({ plain: true }));
+      res.render("homepage", {
+        pets,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
