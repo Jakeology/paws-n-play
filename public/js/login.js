9b5001b01feccf359778e243a58bdca333732290
Jake <xxjakexx14@gmail.com>
2021-12-05 22:07:20 -0500

    added login script file to fetch user login

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,60 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+async function loginFormHandler(event) {
+  event.preventDefault();
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
+  const emailInput = document.getElementById("email-login");
+  const passwordInput = document.getElementById("password-login");
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (email && password) {
+    await fetch("/api/users/login", {
+      method: "post",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then(function (object) {
+        if (object === undefined) {
+          return;
+        }
+
+        if (object.success) {
+          return document.location.replace("/dashboard");
+        }
+
+        if (object.type === "INVALID_EMAIL") {
+          let instance = tippy(emailInput);
+          instance.setProps({
+            arrow: true,
+            placement: "bottom",
+            content: object.message,
+            trigger: "none",
+            theme: "error",
+          });
+          instance.show();
+        }
+        if (object.type === "INVALID_PASS") {
+          let instance = tippy(passwordInput);
+          instance.setProps({
+            arrow: true,
+            placement: "bottom",
+            content: object.message,
+            trigger: "none",
+            theme: "error",
+          });
+          instance.show();
+        }
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }
+}
+
+document.querySelector(".login-form").addEventListener("submit", loginFormHandler);
